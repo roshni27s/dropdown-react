45a051ab6ac89e8da7c662d9daf5cbfff394e733
Roshni Singh <roshni.rx@gmail.com>
2022-11-20 23:16:41 +0530

    dropdown

---
+++ src/components/Dropdown.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import { MenuItems } from './MenuItems'
+import { Link } from 'react-router-dom'
+import './Dropdown.css'
+
+function Dropdown() {
+    return (
+        <>
+        <h1>yoo</h1>
+        </>
+    )
+}
+
+export default Dropdown;
