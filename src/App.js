60ec92de47b9214cbc5d257b9b56def60bebbd7f
Roshni Singh <roshni.rx@gmail.com>
2022-11-21 20:24:47 +0530

    final code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import Navbar from './components/Navbar'
 import './App.css';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router,Route, Routes  } from 'react-router-dom';
+import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import ContactUs from './components/pages/ContactUs';
+import SignUp from './components/pages/SignUp';
+import Marketing from './components/pages/Marketing';
+import Consulting from './components/pages/Consulting';
 
 function App() {
   return (
     
     <Router>
     <Navbar/>
+    <Routes>
+      <Route path='/' exact element={<Home/>}/>
+      <Route path='/services' exact element={<Services/>}/>
+      <Route path='/products' exact element={<Products/>}/>
+      <Route path='/contact-us' exact element={<ContactUs/>}/>
+      <Route path='/sign-up' exact element={<SignUp/>}/>
+      <Route path='/marketing' exact element={<Marketing/>}/>
+      <Route path='/consulting' exact element={<Consulting/>}/>
+    </Routes>
     </Router>
     
   );
--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -4,9 +4,22 @@ import { Link } from 'react-router-dom'
 import './Dropdown.css'
 
 function Dropdown() {
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
     return (
         <>
-        <h1>yoo</h1>
+        <ul onClick={handleClick} className={click ? 'dropdown-menu clicked' : 'dropdown-menu'}>
+            {MenuItems.map((item, index) => {
+                return (
+                    <li key={index}>
+                        <Link className={item.cName} to={item.path} onClick={() => setClick(false)}>
+                            {item.title}
+                        </Link>
+
+                    </li>
+                )
+            })}
+        </ul>
         </>
     )
 }
